88d593143472636768a005d7d38748304ca72ab9
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2020-08-25 12:38:59 +0200

    Added Navigation

--- App.js
+++ App.js
@@ -7,11 +7,11 @@ import ComponentScreen from './src/screens/ComponentScreen';
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Component: ComponentScreen,
+    Components: ComponentScreen,
     List: ListScreen
   },
   {
-    initialRouteName: "List",
+    initialRouteName: "Home",
     defaultNavigationOptions: {
       title: "App"
     }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button,TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}> my homeScreen</Text>;
+const HomeScreen = ({navigation}) => {
+  return <View>
+    <Text style={styles.text}>my homeScreen</Text>
+    <Button 
+      title="Go to Components Demo"
+      onPress={()=>navigation.navigate("Components")}  
+      />
+         <Button 
+      title="Go to CompListonents Demo"
+      onPress={()=>navigation.navigate("List")}  
+      /> 
+
+  </View>;
 };
 
 const styles = StyleSheet.create({
